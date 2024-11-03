396d59d7d12e41e56aa95c3cfff263a801c7c18b
Robbie Redfearn <rredfearn32@gmail.com>
2024-11-03 23:39:33 +0100

    Import ConfigModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule } from 'nestjs-pino';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [LoggerModule.forRoot()],
+  imports: [
+    LoggerModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
   exports: [],
