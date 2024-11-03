9e55da7ace4ccb341dff77a0efb3b0ff84050bcd
Robbie Redfearn <rredfearn32@gmail.com>
2024-11-03 23:41:35 +0100

    Create AuthModule

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,52 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async register(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    console.log(dto);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+          name: dto.name,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+
+      throw error;
+    }
+  }
+
+  async login(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    const doesPasswordMatch = await argon.verify(user.password, dto.password);
+
+    if (!doesPasswordMatch)
+      throw new ForbiddenException('Credentials incorrect');
+
+    return user;
+  }
+}
