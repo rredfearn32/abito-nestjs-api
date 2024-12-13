b65efb2f1ab288fbcc5f53ae73dc42000a71a778
Robbie Redfearn <robbie.redfearn@produktmacher.com>
2024-12-14 00:48:05 +0100

    Add Swagger

--- dist/main.js
+++ dist/main.js
@@ -2,8 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const swagger_1 = require("@nestjs/swagger");
+const path_1 = require("path");
+const express = require("express");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.use('/swagger-static', express.static((0, path_1.join)(__dirname, '../node_modules/swagger-ui-dist')));
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('Abito API')
+        .setDescription('An API for Abito.dev')
+        .setVersion('0.1')
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api', app, document);
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Serve Swagger UI assets
+  app.use(
+    '/swagger-static',
+    express.static(join(__dirname, '../node_modules/swagger-ui-dist')),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Abito API')
+    .setDescription('An API for Abito.dev')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Use static assets URL
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
